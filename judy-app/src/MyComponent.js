b57be4483217f9b8a182d40ec0ded0eda16df0c5
Clara <52570128+clarakuempel@users.noreply.github.com>
2024-07-22 18:47:59 +0200

    setup react app

---
+++ judy-app/src/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyComponent = () => {
+    return (
+      <div>
+        <h1>Hello this my First React Component</h1>
+      </div>
+    );
+  };
+
+export default MyComponent;
